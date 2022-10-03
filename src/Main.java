65f306787f2714544e6c7ff664ce12ae5294dfe4
YuryShageev <schageev.yur@yandex.ru>
2022-10-04 00:46:02 +0300

    3-й блок заданий

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,24 @@ public class Main {
             total = total + salary;
             System.out.println("Месяц " + i + " Итого " + total);
         }
+
+        //Задача 1
+        System.out.println("Task 1");
+        int salaryMe = 29000;
+        int totalMe = 0;
+        for (int i = 1; i <= 12; i++) {
+            totalMe = totalMe + salaryMe;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalMe + " рублей");
+        }
+
+        //Задача 2
+        System.out.println("Task 2");
+        int salaryMeAgain = 29000;
+        int totalMeAgain = 0;
+        for (int i = 1; i <= 12; i++) {
+            totalMeAgain = totalMeAgain + totalMeAgain/100;
+            totalMeAgain = totalMeAgain + salaryMeAgain;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalMeAgain + " рублей");
+        }
     }
 }
