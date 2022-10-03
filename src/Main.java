0742939ac85c898eb3e99f441280c7218721de22
YuryShageev <schageev.yur@yandex.ru>
2022-10-04 00:26:31 +0300

    3-й блок заданий

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Пример
+        System.out.println("Example");
+        int salary = 65535;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
     }
 }
